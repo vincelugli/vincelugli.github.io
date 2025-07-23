2e68a5f6db23b102c21afaffabfb2abdabc130b4
Vince Lugli <lugli@google.com>
2025-07-23 10:13:10 -0700

    Use HashRouter instead of BrowserRouter to get around github pages SPA limitations (can't navigate directly to pages that don't have .html).

--- tournament-app/src/App.tsx
+++ tournament-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 
 import Tournament from './components/Tournament';
